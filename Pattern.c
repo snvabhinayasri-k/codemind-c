02e432b1917d8a9a51cd09db9154b2b2cf422b84
k snvabhinayasri <121416131+snvabhinayasri-k@users.noreply.github.com>
2023-01-24 20:52:42 +0530

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
+
+
+
